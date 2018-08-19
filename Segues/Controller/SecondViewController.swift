36b547e9d6486b4671690b570899eceea55d160b
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-19 14:32:17 -0700

    first commit

---
+++ Segues/Controller/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  Segues
+//
+//  Created by Kevin Lee on 8/19/18.
+//  Copyright © 2018 Kevin Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    var textPassedOver : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = textPassedOver
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
